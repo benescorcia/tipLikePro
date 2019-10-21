fa541e9bb5445cd80de517554d98df8668589173
Seifu <dagnachew@gmail.com>
2019-10-21 14:24:54 -0500

    Brian edits

--- server.js
+++ server.js
@@ -56,135 +56,137 @@ app.use(function(req, res, next){
 // ROUTES
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-// Index
-app.get("/", function(req, res){
-    res.render("index");
-});
+// // Index
+// app.get("/", function(req, res){
+//     res.render("index");
+// });
 
-// Dashboard
-app.get("/dashboard", isSignedIn, function(req, res){
-    res.render("dashboard");
-});
+// // Dashboard
+// app.get("/dashboard", isSignedIn, function(req, res){
+//     res.render("dashboard");
+// });
 
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// Photo ROUTES
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// //+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// // Photo ROUTES
+// //+++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-app.get("/photos", isSignedIn, function(req, res){
+// app.get("/photos", isSignedIn, function(req, res){
 
-    // find all photos
-    Photo.find({}, function(err, photos){
-        if(err){
-            console.log(err);
-        } else {
-            res.render("photos/photos", {photos: photos, currentUser: req.user});
-        }
-    });
-});
+//     // find all photos
+//     Photo.find({}, function(err, photos){
+//         if(err){
+//             console.log(err);
+//         } else {
+//             res.render("photos/photos", {photos: photos, currentUser: req.user});
+//         }
+//     });
+// });
 
-// Create and add photo
-app.post("/photos", function(req, res){ 
+// // Create and add photo
+// app.post("/photos", function(req, res){ 
        
-    //get data from user
-    var image = req.body.image;
-    var created = req.body.created;
-    var newPhoto = {image: image, created: created};
+//     //get data from user
+//     var image = req.body.image;
+//     var created = req.body.created;
+//     var newPhoto = {image: image, created: created};
     
-    Photo.create(newPhoto, function(err, newnew){
-        if(err){
-            console.log(err);
-        } else {
-            //redirect to photos page
-            res.redirect("/photos");
-        }
-    });
-});
+//     Photo.create(newPhoto, function(err, newnew){
+//         if(err){
+//             console.log(err);
+//         } else {
+//             //redirect to photos page
+//             res.redirect("/photos");
+//         }
+//     });
+// });
 
-// New photo/form view
-app.get("/photos/new", isSignedIn, function(req, res){
-    res.render("photos/new");
-});
+// // New photo/form view
+// app.get("/photos/new", isSignedIn, function(req, res){
+//     res.render("photos/new");
+// });
 
-// Show photo
-app.get("/photos/:id", isSignedIn, function(req, res){
-    Photo.findById(req.params.id, function(err, found){
-        if(err){
-            console.log(err)
-        } else {
-            res.render("photos/show", {photo: found});
-        }
-    })
-});
+// // Show photo
+// app.get("/photos/:id", isSignedIn, function(req, res){
+//     Photo.findById(req.params.id, function(err, found){
+//         if(err){
+//             console.log(err)
+//         } else {
+//             res.render("photos/show", {photo: found});
+//         }
+//     })
+// });
 
-// Delete photo
-app.delete("/photos/:id", isSignedIn, function(req, res){
-    Photo.findByIdAndRemove(req.params.id, function(err){
-        if(err){
-            res.redirect("/photos");
-        } else {
-            res.redirect("/photos");
-        }
-    })
-});
-
-
+// // Delete photo
+// app.delete("/photos/:id", isSignedIn, function(req, res){
+//     Photo.findByIdAndRemove(req.params.id, function(err){
+//         if(err){
+//             res.redirect("/photos");
+//         } else {
+//             res.redirect("/photos");
+//         }
+//     })
+// });
 
 
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// Authentication ROUTES
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-// show sign up form
-app.get("/register", function(req, res){
-    res.render("register");
-});
-
-// register user
-app.post("/register", function(req, res){
-    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
-        if(err) {
-            console.log(err);
-            return res.render("register");
-        }
-        passport.authenticate("local")(req, res, function(){
-            res.redirect("/dashboard");
-        });
-    });
-});
 
 
-// show login form
-app.get("/login", function(req, res){
-    res.render("login");
-});
+// //+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// // Authentication ROUTES
+// //+++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-app.post("/login", passport.authenticate("local", {
-    successRedirect: "/dashboard",
-    failureRedirect: "/login"
-}), function(req, res){
+// // show sign up form
+// app.get("/register", function(req, res){
+//     res.render("register");
+// });
+
+// // register user
+// app.post("/register", function(req, res){
+//     User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+//         if(err) {
+//             console.log(err);
+//             return res.render("register");
+//         }
+//         passport.authenticate("local")(req, res, function(){
+//             res.redirect("/dashboard");
+//         });
+//     });
+// });
+
+
+// // show login form
+// app.get("/login", function(req, res){
+//     res.render("login");
+// });
+
+// app.post("/login", passport.authenticate("local", {
+//     successRedirect: "/dashboard",
+//     failureRedirect: "/login"
+// }), function(req, res){
     
-});
+// });
 
 
-// logout
-app.get("/logout", function(req, res){
-    req.logout();
-    res.redirect("/");
-});
+// // logout
+// app.get("/logout", function(req, res){
+//     req.logout();
+//     res.redirect("/");
+// });
 
 
-//isSignedIn Middleware
+// //isSignedIn Middleware
 
-function isSignedIn(req, res, next){
-    if(req.isAuthenticated()){
-        return next();
-    }
-    res.redirect("/login");
-}
+// function isSignedIn(req, res, next){
+//     if(req.isAuthenticated()){
+//         return next();
+//     }
+//     res.redirect("/login");
+// }
 
+var path = require('path');
 
+var x = path.join('Users', 'Refsnes', 'demo_path.js');
 app.use("*", function(req, res){
-    res.sendFile("./tiplikepro-app/build/index.html");
+    res.sendFile( path.join(__dirname, 'tiplikepro-app', 'build', 'index.html'));
 });
 
 
